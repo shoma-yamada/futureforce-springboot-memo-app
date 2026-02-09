a8005e4520e749349516a8ed6ef6648a85fd0757
shomayamada <s.yamada.webdesign@gmail.com>
2026-02-09 16:45:09 +0900

    Memo.javaにNotNullのアノテーション導入

--- src/main/java/com/lesson/memo/model/Memo.java
+++ src/main/java/com/lesson/memo/model/Memo.java
@@ -10,6 +10,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
@@ -37,7 +38,9 @@ public class Memo {
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime updatedAt;
     
+    @NotNull(message = "優先度を選択してください")
     @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private Priority priority;
 
     
