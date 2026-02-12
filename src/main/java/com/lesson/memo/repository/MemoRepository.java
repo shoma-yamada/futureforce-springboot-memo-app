1d120e4d4d7b547eaec40de86946aa5c5f43422b
shomayamada <s.yamada.webdesign@gmail.com>
2026-02-12 23:23:57 +0900

    Repositoryの設定

--- src/main/java/com/lesson/memo/repository/MemoRepository.java
+++ src/main/java/com/lesson/memo/repository/MemoRepository.java
@@ -10,9 +10,9 @@ public interface MemoRepository extends JpaRepository<Memo, Long> {
 	
 	List<Memo> findAllByOrderByUpdatedAtDesc();
 	
-//	List<Memo> findByTitleContainingOrContentContainingOrderByUpdatedAtDesc(
-//			String titleKeyword,
-//			String contentKeyword
-//			);
+	List<Memo> findByTitleContainingOrContentContainingOrderByUpdatedAtDesc(
+			String titleKeyword,
+			String contentKeyword
+			);
     
 }
