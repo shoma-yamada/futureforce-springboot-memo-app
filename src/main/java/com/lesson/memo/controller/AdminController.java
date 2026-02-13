a9f312237a70ddf0c37351114e5a857627d022e1
shomayamada <s.yamada.webdesign@gmail.com>
2026-02-14 02:11:07 +0900

    PostMapping作成

---
+++ src/main/java/com/lesson/memo/controller/AdminController.java
@@ -0,0 +1,39 @@
+package com.lesson.memo.controller;
+
+import jakarta.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.lesson.memo.model.Admin;
+import com.lesson.memo.repository.AdminRepository;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController{
+	
+	@Autowired
+	private AdminRepository adminRepository;
+	
+	@PostMapping("/signup")
+	public String signUp(@ModelAttribute @Valid Admin admin,
+	BindingResult result,
+	Model model) {
+		 if(result.hasErrors()) {
+		        model.addAttribute("admin", admin);
+		        return "admin/signup";
+		    }
+		 
+		 admin.setPassword(passwordEncoder.encode(admin.getPassword()));
+
+		 adminRepository.save(admin);
+		    return "redirect:/memo";
+	}
+
+	
+}
