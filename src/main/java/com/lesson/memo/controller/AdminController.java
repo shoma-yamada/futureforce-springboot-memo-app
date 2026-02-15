4f4c86be3e9db03a915cc3bedbd14b8042b70d4c
shomayamada <s.yamada.webdesign@gmail.com>
2026-02-15 16:06:01 +0900

    SecurityConfig.javaを作成 AdminControllerにimportし、BCryptPasswordEncoder設定

--- src/main/java/com/lesson/memo/controller/AdminController.java
+++ src/main/java/com/lesson/memo/controller/AdminController.java
@@ -3,6 +3,7 @@ package com.lesson.memo.controller;
 import jakarta.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -19,6 +20,7 @@ public class AdminController{
 	
 	@Autowired
 	private AdminRepository adminRepository;
+	private BCryptPasswordEncoder passwordEncoder;
 	
 	@PostMapping("/signup")
 	public String signUp(@ModelAttribute @Valid Admin admin,
